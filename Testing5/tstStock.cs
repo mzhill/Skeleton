a38f34e8ff1fe44d88fa8f32915b187254a3d78b
Mohammed Zhill Rafic <P2587656@my365.dmu.ac.uk>
2022-02-11 14:01:24 +0000

    Added testing for stock class

---
+++ Testing5/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AStock);
+
+        }
+    }
+}
