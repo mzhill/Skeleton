a0b74bb7c5a217c75854c066db4804fb0f8a9220
Mohammed Zhill Rafic <P2587656@my365.dmu.ac.uk>
2022-02-15 17:34:30 +0000

    Added more testing for all the attributes in the stock table

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Availability { get; set; }
+        public DateTime InitialReleaseDate { get; set; }
+        public int ShoeID { get; set; }
+        public string ShoeDescription { get; set; }
+        public string ShoeColour { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- Testing5/tstStock.cs
+++ Testing5/tstStock.cs
@@ -1,6 +1,6 @@
-﻿using ClassLibrary;
+﻿using System;
+using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace Testing5
 {
@@ -14,7 +14,84 @@ namespace Testing5
             clsStock AStock = new clsStock();
             //test to see that it exists
             Assert.IsNotNull(AStock);
+        }
 
+        [TestMethod]
+        public void ShoeIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.ShoeID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ShoeID, TestData);
+        }
+
+        [TestMethod]
+        public void ShoeDescriptionPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data
+            String TestData = "ExampleText";
+            //assign the data to the property
+            AStock.ShoeDescription = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ShoeDescription, TestData);
+        }
+
+        [TestMethod]
+        public void ShoeColourPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data
+            String TestData = "White";
+            //assign the data to the property
+            AStock.ShoeColour = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.ShoeColour, TestData);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AStock.Quantity = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Quantity, TestData);
+        }
+
+        [TestMethod]
+        public void AvailabilityPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data
+            Boolean TestData = true;
+            //assign the data to the property
+            AStock.Availability = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.Availability, TestData);
+        }
+
+        [TestMethod]
+        public void InitialReleaseDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStock.InitialReleaseDate = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.InitialReleaseDate, TestData);
         }
     }
 }
