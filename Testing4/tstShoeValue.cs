6592b6d166467a93196e24553e52936c0b94371c
Kush Modessa <P2587459@my365.dmu.ac.uk>
2022-02-22 18:22:25 +0000

    Added testing for Shoe Value

---
+++ Testing4/tstShoeValue.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstShoeValue
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsShoeValue AShoeValue = new clsShoeValue();
+            Assert.IsNotNull(AShoeValue);
+        }
+    }
+}
