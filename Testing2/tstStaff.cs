6bbe1a0b806175a5e3a4720ddaf733aaea3581d5
Hasan Namik <P2604343@my365.dmu.ac.uk>
2022-02-22 12:49:42 +0000

    Finished testing

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff AnStaff = new clsStaff();
+            Assert.IsNotNull(AnStaff);
+        }
+    }
+}
