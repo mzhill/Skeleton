b732a0ccd6391ebded3b7c2ddddbfda0f2b05ee6
Mohammed Zhill Rafic <P2587656@my365.dmu.ac.uk>
2022-02-22 12:21:49 +0000

    Almost completed lab week 21

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of the class
+        clsStock AStock = new clsStock();
+        //get the data from the session object
+        AStock = (clsStock)Session["AStock"];
+        //display the shoe description on the page
+        Response.Write(AStock.ShoeDescription);
     }
 }
