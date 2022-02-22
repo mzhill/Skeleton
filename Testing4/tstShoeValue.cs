2160d5dee0bc8b3c09e7eba158d724597e9f0137
Kush Modessa <P2587459@my365.dmu.ac.uk>
2022-02-22 17:49:49 +0000

    Added testing for ShoeValue

---
+++ Testing4/tstShoeValue.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstShoeValue
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsShoeValue AnShoeValue = new clsShoeValue();
+            //test to see that it exist
+            Assert.IsNotNull(AnShoeValue);
+        }
+    }
+}
