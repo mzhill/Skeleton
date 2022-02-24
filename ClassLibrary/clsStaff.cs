68b9a86bbb8c00d169555d2103a965357c33d9e9
Hasan Namik <P2604343@my365.dmu.ac.uk>
2022-02-24 10:49:34 +0000

    Made some incomplete tests,will finish later

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -2,5 +2,11 @@
 {
     public class clsStaff
     {
+        public bool CurrentlyWorking { get; set; }
+        public DateTime DateJoined { get; set; }
+        public int StaffID { get; set; }
+        public string Position { get; set; }
+        public string StaffName { get; set; }
+        public int StaffTel { get; set; }
     }
 }
--- Testing2/tstStaff.cs
+++ Testing2/tstStaff.cs
@@ -10,8 +10,74 @@ namespace Testing2
         [TestMethod]
         public void InstanceOK()
         {
-            clsStaff AnStaff = new clsStaff();
-            Assert.IsNotNull(AnStaff);
+            clsStaff AStaff = new clsStaff();
+            Assert.IsNotNull(AStaff);
         }
+
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.StaffID = TestData;
+            Assert.AreEqual(AStaff.ShaffID, TestData);
+        }
+
+        [TestMethod]
+        public void StaffNamePropertyOK()
+        {
+
+            clsStaff AStaff = new clsStaff();
+            String TestData = "ExampleText";
+            AStaff.StaffName = TestData;
+            Assert.AreEqual(AStaff.StaffName, TestData);
+        }
+        [TestMethod]
+        public void PositionPropertyOK()
+        {
+            clsStock AStaff = new clsStaff();
+            String TestData = "Name";
+            AStaff.Position = TestData;
+            Assert.AreEqual(AStaff.Position, TestData);
+        }
+
+        [TestMethod]
+        public void StaffTelPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.StaffTel = TestData;
+            Assert.AreEqual(AStaff.StaffTel, TestData);
+        }
+        [TestMethod]
+        public void StaffSalaryPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Int32 TestData = 1;
+            AStaff.StaffSalary = TestData;
+            Assert.AreEqual(AStaff.StaffSalary, TestData);
+        }
+        [TestMethod]
+        public void CurrentlyWorkingPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            Boolean TestData = true;
+            AStaff.CurrentlyWorking = TestData;
+            Assert.AreEqual(AStaff.CurrentlyWorking], TestData);
+        }
+        [TestMethod]
+        public void DateJoinedPropertyOK()
+        {
+            clsStaff AStaff = new clsStaff();
+            DateTime TestData = DateTime.Now.Date;
+            AStaff.DateJoined = TestData;
+            Assert.AreEqual(AStaff.DateJoined, TestData);
+        }
+
     }
+        
+    }
+
+   
+
 }
