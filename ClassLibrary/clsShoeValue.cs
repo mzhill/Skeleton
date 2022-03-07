cc77d223814e3a00208a7d3b9f62de3d9a482e3b
Kush Modessa <P2587459@my365.dmu.ac.uk>
2022-03-07 18:22:18 +0000

    part 2 week23

--- ClassLibrary/clsShoeValue.cs
+++ ClassLibrary/clsShoeValue.cs
@@ -4,9 +4,49 @@ namespace ClassLibrary
 {
     public class clsShoeValue
     {
-        public int RetailPrice { get; set; }
-        public int ShoeID { get; set; }
-        public int ResalePrice { get; set; }
+        //private data member for the address no popperty
+        private Int32 mShoeID;
+
+        //private data member for the address no popperty
+        private Int32 mRetailPrice;
+
+        //private data member for the address no popperty
+        private Int32 mResaleDate;
+
+        //private data member for the address no popperty
+        private Int32 mCatergory;
+
+        //private data member for the address no popperty
+        private Int32 mStock;
+
+        public int ShoeID
+        {
+            get
+            {
+                //this line of code sends data out of the poperty
+                return mShoeID;
+            }
+            set
+            {
+                //this line of code allows data into the poperty
+                mShoeID = value;
+            }
+        }
+        public int RetailPrice
+        {
+            get
+            {
+                //this line of code sends data out of the poperty
+                return mRetailPrice;
+            }
+            set
+            {
+
+            }
+        }
+        public int ResalePrice 
+
+
         public int Catergory { get; set; }
         public int ReleaseDate { get; set; }
         public int Stock { get; set; }
