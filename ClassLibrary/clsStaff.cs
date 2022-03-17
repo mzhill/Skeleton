664665564fd3a8d1cbafa1e8ec1b72c3449564d8
Namik02 <hasannamik02@outlook.com>
2022-03-17 10:51:23 +0000

    started week 23

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,100 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public bool CurrentlyWorking { get; set; }
-        public DateTime DateJoined { get; set; }
-        public int StaffID { get; set; }
-        public string Position { get; set; }
-        public string StaffName { get; set; }
-        public int StaffTel { get; set; }
-        public int StaffSalary { get; set; }
+        private Int32 mStaffID;
+        private DateTime mDateJoined;
+        private Boolean mCurrentlyWorking;
+        private string mPosition;
+        private string mStaffName;
+        private Int32 mStaffTel;
+        private Int32 mStaffSalary;
+
+        public bool CurrentlyWorking
+        {
+            get
+            {
+                return mCurrentlyWorking;
+            }
+            set
+            {
+                mCurrentlyWorking = value;
+            }
+        }
+
+        public DateTime DateJoined
+        {
+            get
+            {
+                return mDateJoined;
+            }
+            set
+            {
+                mDateJoined = value;
+            }
+        }
+
+        public Int32 StaffID
+        {
+            get
+            {
+                return mStaffID;
+            }
+            set
+            {
+                mStaffID = value;
+            }
+        }
+
+        public string Position
+        {
+            get
+            {
+                return mPosition;
+            }
+            set
+            {
+                mPosition = value;
+            }
+        }
+
+        public string StaffName
+        {
+            get
+            {
+                return mStaffName;
+            }
+            set
+            {
+                mStaffName = value;
+            }
+        }
+
+        public Int32 StaffSalary
+        {
+            get
+            {
+                return mStaffSalary;
+            }
+            set
+            {
+                mStaffSalary = value;
+            }
+        }
+        public Int32 StaffTel
+        {
+            get
+            {
+                return mStaffTel;
+            }
+            set
+            {
+                mStaffTel = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            return true;
+        }
     }
 }
