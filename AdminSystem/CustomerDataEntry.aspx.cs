684ca42a5272de92d4fc49a4fcbdbb65eedb9fa4
Adil Alarakhia <P2592597@my365.dmu.ac.uk>
2022-03-29 15:57:19 +0100

    branchesssss

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -30,4 +30,32 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the address class
+        clsCustomer AnCustomer = new clsCustomer();
+        //variable to store the primary key
+        Int32 CustomerID;
+        //variable to store the result of find operation
+        Boolean Found = false;
+        //get tje primary key entered by the user
+        CustomerID = Convert.ToInt32(txtCustomerID.Text);
+        //find the record
+        Found = AnCustomer.Find(CustomerID);
+        //if found
+        if (Found == true)
+        {
+            //display the values
+            txtCustomerForename.Text = AnCustomer.CustomerForename;
+            txtCustomerSurname.Text = AnCustomer.CustomerSurname;
+            txtCustomerDOB.Text = AnCustomer.CustomerDOB.ToString();
+            txtCustomerEmail.Text = AnCustomer.CustomerEmail;
+            txtCustomerTel.Text = AnCustomer.CustomerTel;
+            chkCustomerMembership.Text = AnCustomer.CustomerMembership.ToString();
+
+
+
+        }
+    }
 }
