243cac113751e280e49d30904367112e3fe24b32
Adil Alarakhia <P2592597@my365.dmu.ac.uk>
2022-04-11 18:47:31 +0100

    adillll;l

--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -1,9 +1,44 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsCustomerCollection
     {
+        //constructor for the class
+        public clsCustomerCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblCustomer_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank stock
+                clsCustomer AnCustomer = new clsCustomer();
+                //read in the fields from the current record
+                AnCustomer.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                AnCustomer.CustomerForename = Convert.ToString(DB.DataTable.Rows[Index]["CustomerForename"]);
+                AnCustomer.CustomerSurname = Convert.ToString(DB.DataTable.Rows[Index]["CustomerSurname"]);
+                AnCustomer.CustomerDOB = Convert.ToDateTime(DB.DataTable.Rows[Index]["CustomerDOB"]);
+                AnCustomer.CustomerEmail = Convert.ToString(DB.DataTable.Rows[Index]["CustomerEmail"]);
+                AnCustomer.CustomerTel = Convert.ToString(DB.DataTable.Rows[Index]["CustomerTel"]);
+                AnCustomer.CustomerMembership = Convert.ToBoolean(DB.DataTable.Rows[Index]["CustomerMembership"]);
+                //add the record to the private data member
+                mCustomerList.Add(AnCustomer);
+                //point at the next record
+                Index++;
+            }
+        }
+
+
         //private data member for the list
         List<clsCustomer> mCustomerList = new List<clsCustomer>();
 
