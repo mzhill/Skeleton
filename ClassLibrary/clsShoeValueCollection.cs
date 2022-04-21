0be74dbc58bd5d6dcf8d443a1df846813046794a
Kush Modessa <P2587459@my365.dmu.ac.uk>
2022-04-21 19:20:24 +0100

    zhill did some

--- ClassLibrary/clsShoeValueCollection.cs
+++ ClassLibrary/clsShoeValueCollection.cs
@@ -1,10 +1,147 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsShoeValueCollection
     {
-        public List<clsShoeValue> ShoeValueList { get; set; }
-        public int Count { get; set; }
+        //private data member for the list
+        List<clsShoeValue> mShoeValueList = new List<clsShoeValue>();
+        //private data member thisStock
+        clsShoeValue mThisShoeValue = new clsShoeValue();
+
+        public clsShoeValueCollection()
+        {
+            //object for data collection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblShoeValue_SelectAll");
+            //populate the array list with the data table
+            PopulateArray(DB);
+        }
+
+        public List<clsShoeValue> ShoeValueList
+        {
+            get
+            {
+                //return the private data
+                return mShoeValueList;
+            }
+            set
+            {
+                //set the private data
+                mShoeValueList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mShoeValueList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+
+        public clsShoeValue ThisShoeValue
+        {
+            get
+            {
+                //return the private
+                return mThisShoeValue;
+            }
+            set
+            {
+                //set the private data
+                mThisShoeValue = value;
+            }
+        }
+
+        public int Add()
+        {
+            //adds a new record to the database based on the values of this mThisStock
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("RetailPrice", mThisShoeValue.RetailPrice);
+            DB.AddParameter("ResalePrice", mThisShoeValue.ResalePrice);
+            DB.AddParameter("Catergory ", mThisShoeValue.Catergory);
+            DB.AddParameter("ReleaseDate", mThisShoeValue.ReleaseDate);
+            DB.AddParameter("Stock", mThisShoeValue.Stock);
+            //execute the query returning the primary key value
+            return mThisShoeValue.ShoeID;
+        }
+
+        public void Update()
+        {
+            //update an existing record based on the values of thisShoeValue
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@RetailPrice", mThisShoeValue.RetailPrice);
+            DB.AddParameter("@ResalePrice", mThisShoeValue.ResalePrice);
+            DB.AddParameter("@Catergory ", mThisShoeValue.Catergory);
+            DB.AddParameter("@ReleaseDate", mThisShoeValue.ReleaseDate);
+            DB.AddParameter("@Stock", mThisShoeValue.Stock);
+            //execute the stored procedure
+            DB.Execute("sproc_tblShoeValue_Update");
+        }
+
+        public void Delete()
+        {
+            //deletes the record pointed to by thisShoeValue
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@ShoeID", mThisShoeValue.ShoeID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblShoeValue_Delete");
+        }
+
+        public void ReportByCatergory(string Catergory)
+        {
+            //filters the records based on a full or partial category
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //send the Catergory parameter to the database
+            DB.AddParameter("@Catergory", Catergory);
+            //execute the stored procedure
+            DB.Execute("sproc_tblShoeValue_FilterByCatergory");
+            //populate the array list with the data lable
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mShoeValueList = new List<clsShoeValue>();
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank record
+                clsShoeValue AnShoeValue = new clsShoeValue();
+                //read in the fields from the current record
+                AnShoeValue.ShoeID = Convert.ToInt32(DB.DataTable.Rows[Index]["ShoeID"]);
+                AnShoeValue.RetailPrice = Convert.ToString(DB.DataTable.Rows[Index]["RetailPrice"]);
+                AnShoeValue.ResalePrice = Convert.ToString(DB.DataTable.Rows[Index]["ResalePrice"]);
+                AnShoeValue.Catergory = Convert.ToString(DB.DataTable.Rows[Index]["Catergory "]);
+                AnShoeValue.ReleaseDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["ReleaseDate"]);
+                AnShoeValue.Stock = Convert.ToBoolean(DB.DataTable.Rows[Index]["Stock"]);
+                //add the record to the private data member
+                mShoeValueList.Add(AnShoeValue);
+                //point at the next record
+                Index++;
+            }
+        }
     }
 }
--- Testing4/tstShoevalueCollection.cs
+++ Testing4/tstShoevalueCollection.cs
@@ -20,7 +20,7 @@ namespace Testing_Framework
         }
 
         [TestMethod]
-        public void ShoeValueInstanceOK()
+        public void ShoeValueListOK()
         {
             //create an instance of the class we want to create 
             clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
@@ -47,19 +47,27 @@ namespace Testing_Framework
         }
 
         [TestMethod]
-        public void CountPropertyOK()
+        public void ThisShoeValuePropertyOK()
         {
-            //create an instance of the class we want to create 
+            //create an instance of the class we want to create
             clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
-            //create some test dT to assign to the property
-            Int32 SomeCount = 0;
+            //create some test data to assign to the property
+            clsShoeValue TestShoeValue = new clsShoeValue();
+            //set the properties of the test object
+            TestShoeValue.ShoeID = 1;
+            TestShoeValue.RetailPrice = "160";
+            TestShoeValue.ResalePrice = "1300";
+            TestShoeValue.Catergory = "Jordan";
+            TestShoeValue.ReleaseDate = DateTime.Now.Date;
+            TestShoeValue.Stock = true;
             //assign the data to the property
-            AllShoeValue.Count = SomeCount;
-            //test to see that the values are the same
-            Assert.AreEqual(AllShoeValue.Count, SomeCount);
+            AllShoeValue.ThisShoeValue = TestShoeValue;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllShoeValue.ThisShoeValue, TestShoeValue);
         }
+
         [TestMethod]
-        public void ShoeValuePropertyOK()
+        public void ListAndCountOK()
         {
             //create an instance of the class we want to create 
             clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
@@ -81,9 +89,160 @@ namespace Testing_Framework
             //Assign the data to the property
             AllShoeValue.ShoeValueList = TestList;
             //test to see that the two value are the same
-            Assert.AreEqual(AllShoeValue.ShoeValueList, TestList);
-
+            Assert.AreEqual(AllShoeValue.Count, TestList.Count);
         }
 
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
+            //create the item of test data
+            clsShoeValue TestItem = new clsShoeValue();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.ShoeID = 1;
+            TestItem.RetailPrice = "160";
+            TestItem.ResalePrice = "1300";
+            TestItem.Catergory = "Jordan";
+            TestItem.ReleaseDate = DateTime.Now.Date;
+            TestItem.Stock = true;
+            //set ThisStock to the test data
+            AllShoeValue.ThisShoeValue = TestItem;
+            //add the record
+            PrimaryKey = AllShoeValue.Add();
+            //set the primary key of the test data
+            TestItem.ShoeID = PrimaryKey;
+            //find the record
+            AllShoeValue.ThisShoeValue.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllShoeValue.ThisShoeValue, TestItem);
+        }
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsShoeValueCollection AllStock = new clsShoeValueCollection();
+            //create some test data to assign to the property
+            clsShoeValue TestItem = new clsShoeValue();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.ShoeID = 1;
+            TestItem.RetailPrice = "160";
+            TestItem.ResalePrice = "1300";
+            TestItem.Catergory = "Jordan";
+            TestItem.ReleaseDate = DateTime.Now.Date;
+            TestItem.Stock = true;
+            //set ThisShoeValue to the test data
+            AllStock.ThisShoeValue = TestItem;
+            //add the record
+            PrimaryKey = AllStock.Add();
+            //set the primary key of the test data
+            TestItem.ShoeID = PrimaryKey;
+            //modify the test data
+            TestItem.ShoeID = 2;
+            TestItem.RetailPrice = "200";
+            TestItem.ResalePrice = "450";
+            TestItem.Catergory = "Jordan";
+            TestItem.ReleaseDate = DateTime.Now.Date;
+            TestItem.Stock = true;
+            //set ThisShoeValue to the test data
+            AllStock.ThisShoeValue = TestItem;
+            //update the record
+            AllStock.Update();
+            //find the record
+            AllStock.ThisShoeValue.Find(PrimaryKey);
+            //test to see ThisShoeValue matches the test data
+            Assert.AreEqual(AllStock.ThisShoeValue, TestItem);
+        }
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create 
+            clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
+            //create the item of test data
+            clsShoeValue TestItem = new clsShoeValue();
+            //var to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.ShoeID = 1;
+            TestItem.RetailPrice = "160";
+            TestItem.ResalePrice = "1300";
+            TestItem.Catergory = "Jordan";
+            TestItem.ReleaseDate = DateTime.Now.Date;
+            TestItem.Stock = true;
+            //set ThisStock to the test data
+            AllShoeValue.ThisShoeValue = TestItem;
+            //add the record
+            PrimaryKey = AllShoeValue.Add();
+            //set the primary key of the test data
+            TestItem.ShoeID = PrimaryKey;
+            //delete the record
+            AllShoeValue.Delete();
+            //find the record
+            AllShoeValue.ThisShoeValue.Find(PrimaryKey);
+            //now find the record
+            Boolean Found = AllShoeValue.ThisShoeValue.Find(PrimaryKey);
+            //test to see that the record was not found
+            Assert.IsFalse(Found);
+        }
+
+        [TestMethod]
+        public void ReportByCatergoryMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsShoeValueCollection AllShoeValue = new clsShoeValueCollection();
+            //create an instance of the filtered data
+            clsShoeValueCollection FilteredShoeValue = new clsShoeValueCollection();
+            //apply a blank string (should return all records)
+            FilteredShoeValue.ReportByCatergory("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllShoeValue.Count, FilteredShoeValue.Count);
+        }
+
+        public void ReportByCatergoryNoneFound()
+        {
+            //create an instance of the filtered data
+            clsShoeValueCollection FilteredShoeValue = new clsShoeValueCollection();
+            //apply a blank string (should return all records)
+            FilteredShoeValue.ReportByCatergory("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredShoeValue.Count);
+        }
+
+        [TestMethod]
+        public void ReportByCatergoryTestDataFound()
+        {
+            //create an instance of the class containing unfiltered results
+            clsShoeValueCollection FilteredShoeValue = new clsShoeValueCollection();
+            //var to store outcome
+            Boolean OK = true;
+            //apply a shoe colour that does not exist
+            FilteredShoeValue.ReportByCatergory("Jordan");
+            //check that the correct number of records are found
+            if (FilteredShoeValue.Count == 2)
+            {
+                //check that the first record is ID 4
+                if (FilteredShoeValue.ShoeValueList[0].ShoeID != 4)
+                {
+                    OK = false;
+                }
+                //check that the first record is ID 5
+                if (FilteredShoeValue.ShoeValueList[1].ShoeID != 5)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            //test to see that there are no records
+            Assert.IsTrue(OK);
+        }
     }
 }
