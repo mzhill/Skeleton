5b238f6da03464ff8d9587aacf84cc7384ef1ede
Mohammed Zhill Rafic <P2587656@my365.dmu.ac.uk>
2022-04-28 17:11:46 +0100

    completed shoevaluedataentry

--- AdminSystem/ShoeValueDataEntry.aspx.cs
+++ AdminSystem/ShoeValueDataEntry.aspx.cs
@@ -9,15 +9,38 @@ using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+    //variable to store the primary key with page level scope
+    Int32 ShoeID;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //get the number of the shoevalue to be processed
+        ShoeID = Convert.ToInt32(Session["ShoeID"]);
+        if (IsPostBack == false)
+        {
+            //if this is not a new record
+            if (ShoeID != -1)
+            {
+                //display the current data for the record
+                DisplayShoeValue();
+            }
+        }
     }
 
-    protected void txtRetailPrice_TextChanged(object sender, EventArgs e)    {
-
+    private void DisplayShoeValue()
+    {
+        //create a new instance of the shoe value collection
+        clsShoeValueCollection ShoeValueList = new clsShoeValueCollection();
+        //find the record to update
+        ShoeValueList.ThisShoeValue.Find(ShoeID);
+        //display the data for this record
+        txtShoeID.Text = ShoeValueList.ThisShoeValue.ShoeID.ToString();
+        txtRetailPrice.Text = ShoeValueList.ThisShoeValue.RetailPrice.ToString();
+        txtResalePrice.Text = ShoeValueList.ThisShoeValue.ResalePrice.ToString();
+        txtCatergory.Text = ShoeValueList.ThisShoeValue.Catergory.ToString();
+        txtReleaseDate.Text = ShoeValueList.ThisShoeValue.ReleaseDate.ToString();
+        chkStock.Checked = ShoeValueList.ThisShoeValue.Stock;
     }
-
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
@@ -57,27 +80,43 @@ public partial class _1_DataEntry : System.Web.UI.Page
             //capture the shoe availability
             AnShoeValue.Stock = Convert.ToBoolean(chkStock.Checked);
 
-            //store the stock in the session object
-            Session["AShoeValue"] = AnShoeValue;
-            //navigate to the viewer page
-            Response.Redirect("ShoeValueViewer.aspx");
+            //create a new instance of the shoe value collection
+            clsShoeValueCollection ShoeValueList = new clsShoeValueCollection();
+
+            //if this is a new record i.e. ShoeID = -1 then add the data
+            if (Convert.ToInt32(txtShoeID.Text) == -1)
+            {
+                //set the ThisShoeValue property
+                ShoeValueList.ThisShoeValue = AnShoeValue;
+                //add the new record
+                ShoeValueList.Add();
+            }
+            //otherwise it must be an update
+            else
+            {
+                //find the record to update
+                ShoeValueList.ThisShoeValue.Find(Convert.ToInt32(txtShoeID.Text));
+                //set the ThisShoeValue property
+                ShoeValueList.ThisShoeValue = AnShoeValue;
+                //update the record
+                ShoeValueList.Update();
+            }
+
+            //navigate to the list page
+            Response.Redirect("ShoeValueList.aspx");
         }
         else
         {
             //display the error message
             lblError.Text = Error;
         }
-  
-
-
-
     }
 
 
     protected void btnFind_Click(object sender, EventArgs e)
     {
         //create an instance of the address class
-        clsShoeValue AnShoeVlaue = new clsShoeValue();
+        clsShoeValue AnShoeValue = new clsShoeValue();
         //variable to store the primary key 
         Int32 ShoeID;
         //variable to store the result of the find operation
@@ -85,18 +124,26 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //get the primary key entered by the user
         ShoeID = Convert.ToInt32(txtShoeID.Text);
         //find the record
-        Found = AnShoeVlaue.Find(ShoeID);
+        Found = AnShoeValue.Find(ShoeID);
         //if found
         if (Found == true)
         {
             //display the valyes of the properties in the form
-            txtRetailPrice.Text = AnShoeVlaue.ResalePrice;
-            txtResalePrice.Text = AnShoeVlaue.ResalePrice;
-            txtCatergory.Text = AnShoeVlaue.Catergory;
-            txtReleaseDate.Text = AnShoeVlaue.ReleaseDate.ToString();
-            chkStock.Text = AnShoeVlaue.Stock.ToString();
-
+            txtRetailPrice.Text = AnShoeValue.ResalePrice;
+            txtResalePrice.Text = AnShoeValue.ResalePrice;
+            txtCatergory.Text = AnShoeValue.Catergory;
+            txtReleaseDate.Text = AnShoeValue.ReleaseDate.ToString();
+            chkStock.Text = AnShoeValue.Stock.ToString();
+            lblError.Text = "";
         }
-
+        else
+        {
+            txtRetailPrice.Text = "";
+            txtResalePrice.Text = "";
+            txtCatergory.Text = "";
+            txtReleaseDate.Text = "";
+            chkStock.Checked = AnShoeValue.Stock;
+            lblError.Text = "Error: No stock has been found with the Shoe ID " + ShoeID;
+        }
     }
 }
